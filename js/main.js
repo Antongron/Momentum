65bd1f184eb04e047d9e0586057aee2c1bbfb148
Antongron <60682710+Antongron@users.noreply.github.com>
2020-10-25 22:54:30 +0300

    fix: fix links on img

--- js/main.js
+++ js/main.js
@@ -41,15 +41,15 @@ const time = document.getElementById('time'),
 
     if (hour < 12) {
         //Morning
-        document.body.style.backgroundImage = "url('../img/morning.jpg')";
+        document.body.style.backgroundImage = "url('../morning.jpg')";
         greeting.textContent = 'Good Morning';
     } else if (hour < 18) {
         //Afternoon
-        document.body.style.backgroundImage = "url('../img/afternoon.jpg')";
+        document.body.style.backgroundImage = "url('../afternoon.jpg')";
         greeting.textContent = 'Good Afternoon';
     } else {
         //Evening
-        document.body.style.backgroundImage = "url('../img/night.jpg')";
+        document.body.style.backgroundImage = "url('../night.jpg')";
         greeting.textContent = 'Good Evening';
         document.body.style.color = 'white';
     }
