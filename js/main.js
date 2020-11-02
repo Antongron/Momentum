8cab72f3633e7380cb6a1a3f987a951d96cfb26f
Antongron <60682710+Antongron@users.noreply.github.com>
2020-11-02 07:52:11 +0300

    refactor: upload new img, refactoring code

--- js/main.js
+++ js/main.js
@@ -7,8 +7,17 @@ const time = document.getElementById('time'),
 
 
   const montsArr = ["Jannuary", "Frbruary", "March", "April", "May", "June", "July", "August", "September", "October", "Nowember", "December"],
-        nameDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
+        nameDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        baseMorning = 'https://raw.githubusercontent.com/Antongron/Momentum/main/img/morning/',
+        imagesMorning = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg'],
+        baseDay = 'https://raw.githubusercontent.com/Antongron/Momentum/main/img/day/',
+        imagesDay = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg'],
+        baseEvening = 'https://raw.githubusercontent.com/Antongron/Momentum/main/img/evening/',
+        imagesEvening = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg'],
+        baseNight = 'https://raw.githubusercontent.com/Antongron/Momentum/main/img/night/',
+        imagesNight = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg'],
+        btn = document.querySelector('.btn');
+  let i = 0;
   // Options
   const showAmPm = true;
 
@@ -39,98 +48,7 @@ const time = document.getElementById('time'),
       return (parseInt(n, 10) < 10 ? '0' : '') + n;
   }
 
-  //Set Background and Greeting
-  function setBgGreet() {
-    let today = new Date(),
-      hour = today.getHours();
 
-    if (hour > 5 && hour < 12) {
-        //Morning
-        document.body.style.backgroundImage = "url('https://github.com/Antongron/Momentum/blob/gh-pages/img/afternoon.jpg?raw=true')";
-        greeting.textContent = 'Good Morning';
-    } else if (hour >= 12 && hour < 18) {
-        //Afternoon
-        document.body.style.backgroundImage = "url('https://github.com/Antongron/Momentum/blob/gh-pages/img/afternoon.jpg?raw=true')";
-        greeting.textContent = 'Good Afternoon';
-    } else if (hour>= 18 && hour < 24) {
-        //Evening
-        document.body.style.backgroundImage = "url('https://github.com/Antongron/Momentum/blob/gh-pages/img/evening.jpg?raw=true')";
-        greeting.textContent = 'Good Evening';
-    } else {
-        //Night
-        document.body.style.backgroundImage = "url('https://github.com/Antongron/Momentum/blob/gh-pages/img/afternoon.jpg?raw=true')";
-        greeting.textContent = 'Good Night';
-        document.body.style.color = 'white';
-    }
-  };
-
-  //Set Name
-  function setName(e) {
-      if(e.type === 'keypress') {
-          //Make sure enter is pressed
-          if(e.which == 13 || e.keyCode == 13) {
-              localStorage.setItem('name', e.target.innerText);
-              name.blur();
-          }
-      } else {
-          localStorage.setItem('name', e.target.innerText);
-      }
-  };
-
-  //Get Name
-  function getName() {
-      if(localStorage.getItem('name') === null) {
-          name.textContent = ' [Enter Name]';
-      } else {
-          name.textContent = localStorage.getItem('name');
-      }
-  };
-
-    //Get Focus
-    function getFocus() {
-        if(localStorage.getItem('focus') === null) {
-            focus.textContent = ' [Enter Focus]';
-        } else {
-            focus.textContent = localStorage.getItem('focus');
-        }
-    };
-
-
-    //Set Focus
-    function setFocus(e) {
-        if(e.type === 'keypress') {
-            //Make sure enter is pressed
-            if(e.which == 13 || e.keyCode == 13) {
-                localStorage.setItem('focus', e.target.innerText);
-                focus.blur();
-            }
-        } else {
-            localStorage.setItem('focus', e.target.innerText);
-        }
-    };
-    
-
-    name.addEventListener('keypress', setName);
-    name.addEventListener('blur', setName);
-    focus.addEventListener('keypress', setFocus);
-    focus.addEventListener('blur', setFocus);
-
-  //Run
-  showTime();
-  setBgGreet();
-  getName();
-  getFocus();
-
-    const baseMorning = 'https://raw.githubusercontent.com/Antongron/Momentum/main/img/morning/',
-        imagesMorning = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg'],
-        baseDay = 'https://raw.githubusercontent.com/Antongron/Momentum/main/img/day/',
-        imagesDay = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg'],
-        baseEvening = 'https://raw.githubusercontent.com/Antongron/Momentum/main/img/evening/',
-        imagesEvening = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg'],
-        baseNight = 'https://raw.githubusercontent.com/Antongron/Momentum/main/img/night/',
-        imagesNight = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg'];
-  let i = 0;
-  
   function viewBgImage(data) {
     const body = document.querySelector('body');
     const src = data;
@@ -169,7 +87,105 @@ const time = document.getElementById('time'),
         btn.disabled = true;
         setTimeout(function() { btn.disabled = false }, 1000);
   } 
-  const btn = document.querySelector('.btn');
+
+
+  //Set Background and Greeting
+  function setBgGreet() {
+    let today = new Date(),
+      hour = today.getHours();
+
+    if (hour > 5 && hour < 12) {
+        //Morning
+        getImage();
+        greeting.textContent = 'Good Morning';
+    } else if (hour >= 12 && hour < 18) {
+        //Afternoon
+        getImage();
+        greeting.textContent = 'Good Afternoon';
+    } else if (hour>= 18 && hour < 24) {
+        //Evening
+        document.body.style.backgroundImage = "url('https://github.com/Antongron/Momentum/blob/gh-pages/img/evening.jpg?raw=true')";
+        greeting.textContent = 'Good Evening';
+    } else {
+        //Night
+        document.body.style.backgroundImage = "url('https://github.com/Antongron/Momentum/blob/gh-pages/img/afternoon.jpg?raw=true')";
+        greeting.textContent = 'Good Night';
+        document.body.style.color = 'white';
+    }
+  };
+
+  function getName() {
+    if (localStorage.getItem('name') === null) {
+      name.textContent = '[Введи имя]';
+    } else {
+      name.textContent = localStorage.getItem('name');
+    }
+  }
+  
+  function setName(e) {
+    if (e.type === 'keypress') {
+      if (e.which == 13 || e.keyCode == 13) {
+        if (e.target.innerText.trim().length === 0) {
+          getName();
+          name.blur();
+          return;
+        } else {
+          localStorage.setItem('name', e.target.innerText);
+          name.blur();
+        }
+      }
+    } else {
+      localStorage.setItem('name', e.target.innerText);
+    }
+  }
+  
+  function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+      focus.textContent = '[Введи цель]';
+    } else {
+      focus.textContent = localStorage.getItem('focus');
+    }
+  }
+  
+  function setFocus(e) {
+    if (e.type === 'keypress') {
+      if (e.which == 13 || e.keyCode == 13) {
+        if (e.target.innerText.trim().length === 0) {
+          getFocus();
+          focus.blur();
+          return;
+        } else {
+          localStorage.setItem('focus', e.target.innerText);
+          focus.blur();
+        }
+      }
+    } else {
+      localStorage.setItem('focus', e.target.innerText);
+    }
+  }
+
+  function clear(elem) {
+  elem.textContent = '';
+}
+    
+
+
+
+  name.addEventListener('keypress', setName);
+  name.addEventListener('focus', () => clear(name));
+  name.addEventListener('blur', getName);
+  focus.addEventListener('keypress', setFocus);
+  focus.addEventListener('focus', () => clear(focus));
+  focus.addEventListener('blur', getFocus);
   btn.addEventListener('click', getImage);
-  setTimeout(getImage, 1000);
+
+  //Run
+  showTime();
+  setBgGreet();
+  getName();
+  getFocus();
+
+
+ 
+  
   setInterval(getImage, 3600000);
